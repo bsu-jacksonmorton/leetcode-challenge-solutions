92b62d245f063bcc0131f91ffe49f578d183ab03
bsu-jacksonmorton <jacksonmorton@u.boisestate.edu>
2023-12-18 09:55:25 -0700

    1913 complete

---
+++ 1913/single-pass.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        big1, small1 = 0, inf
+        big2, small2 = 0, inf
+        for num in nums:
+            if big1 < num:
+                big2 = big1
+                big1 = num
+            elif big2 < num:
+                big2 = num
+            if small1 > num:
+                small2 = small1
+                small1 = num
+            elif small2 > num:
+                small2 = num
+        return (big1 * big2) - (small1 * small2)
