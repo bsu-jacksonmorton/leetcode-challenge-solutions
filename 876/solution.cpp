447ded0b06fd8be032e2ead78f365efd64e0dc8b
Jackson Morton <jacksonmorton@u.boisestate.edu>
2024-03-06 17:24:44 -0700

    876 complete.

---
+++ 876/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	/*
+		Fast and Slow Pointer algorithm for returning the middle node in a linked list.
+		Runtime: O(n)
+		Space: O(1)
+	*/
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
