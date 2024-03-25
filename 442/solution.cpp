4aad52531bbae06aa98db266815fd32c957b1c2e
bsu-jacksonmorton <jacksonmorton@u.boisestate.edu>
2024-03-25 10:31:09 -0600

    442 explanation added.

--- 442/solution.cpp
+++ 442/solution.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    /*
+        We can use the same linked list approach as the find duplicate problem.
+        Due to the fact that each element can only appear twice, we can keep track
+        of elements we have seen by negating elements the first time we visit them.
+        Upon revisitation, we can check if the element has previously been negated and
+        if it has we can add it to our ans list.
+    Runtime: O(n)
+Space: O(1)
+    */
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
         for(int num : nums)
