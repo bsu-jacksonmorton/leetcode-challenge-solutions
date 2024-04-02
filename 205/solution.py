ccb5a2251f8928d5cddac6bd5967a39b8ec093b9
bsu-jacksonmorton <jacksonmorton@u.boisestate.edu>
2024-04-02 11:15:33 -0600

    205 complete.

---
+++ 205/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    '''
+    Runtime: O(n)
+    Space: O(2n) since len(s) == len(t) by problem constraints
+    '''
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        m1 = {}
+        m2 = {}
+        for i in range(len(s)):
+            if s[i] in m1 and m1[s[i]] != t[i]:
+                return False
+            if t[i] in m2 and m2[t[i]] != s[i]:
+                return False
+            m1[s[i]] = t[i]
+            m2[t[i]] = s[i]
+        return True
