58da3fcedfb4dbfac3ab0f558d9c537df596dfef
bsu-jacksonmorton <jacksonmorton@u.boisestate.edu>
2024-04-09 10:58:29 -0600

    2073 complete.

---
+++ 2073/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    '''
+    runtime: O(n)
+    space: O(1)
+    '''
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        ans = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                ans += min(tickets[k], tickets[i])
+            else:
+                ans += min(tickets[k] - 1, tickets[i])
+        return ans
